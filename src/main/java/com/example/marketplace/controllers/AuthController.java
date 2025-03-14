7a3fc00dfd22dfca33e94153f82d301f29efc759
Donattah <d.ajuang@carepay.com>
2025-03-14 06:16:19 +0300

    Add login

--- src/main/java/com/example/marketplace/controllers/AuthController.java
+++ src/main/java/com/example/marketplace/controllers/AuthController.java
@@ -2,10 +2,14 @@ package com.example.marketplace.controllers;
 
 import com.example.marketplace.model.User;
 import com.example.marketplace.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -13,11 +17,22 @@ import org.springframework.web.bind.annotation.*;
 
 public class AuthController {
     private final UserService userService;
+    private final HttpServletResponse httpServletResponse;
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestParam String username,
                                          @RequestParam String email,
                                          @RequestParam String password) {
         return  ResponseEntity.ok(userService.registerUser(username, email, password));
+
     };
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestParam String email,
+                                                     @RequestParam String password) {
+        String token = userService.login(email, password);
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+        return ResponseEntity.ok(response);
+    }
 }
